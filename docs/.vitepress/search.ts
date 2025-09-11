5cfb96022537469be489ad5dce7c6d3a4331ebe1
Isanin Sergey <sergey.isanin+github@gmail.com>
2025-09-11 10:40:14 +0300

    docs(search): обновлены параметры локального поиска
    Упрощена структура экспорта и добавлен параметр provider для настройки локального поиска в документации. Рабочий вариант поиска оставлен без изменений.

--- docs/.vitepress/search.ts
+++ docs/.vitepress/search.ts
@@ -1,23 +1,24 @@
 // .vitepress/search.ts
-import type { DefaultTheme } from 'vitepress'
-
-export const search: DefaultTheme.LocalSearchOptions = {
-  locales: {
-    root: {
-      translations: {
-        button: {
-          buttonText: 'Поиск',
-          buttonAriaLabel: 'Открыть поиск'
-        },
-        modal: {
-          displayDetails: 'Показать подробности',
-          resetButtonTitle: 'Очистить',
-          backButtonTitle: 'Назад',
-          noResultsText: 'Ничего не найдено',
-          footer: {
-            selectText: 'выбрать',
-            navigateText: 'навигация',
-            closeText: 'закрыть'
+export const search = {
+  provider: 'local',
+  options: {
+    locales: {
+      root: {
+        translations: {
+          button: {
+            buttonText: 'Поиск',
+            buttonAriaLabel: 'Открыть поиск'
+          },
+          modal: {
+            displayDetails: 'Показать подробности',
+            resetButtonTitle: 'Очистить',
+            backButtonTitle: 'Назад',
+            noResultsText: 'Ничего не найдено',
+            footer: {
+              selectText: 'выбрать',
+              navigateText: 'навигация',
+              closeText: 'закрыть'
+            }
           }
         }
       }
